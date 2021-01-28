5aa7c93ec27ecbcf0092efdf0acdc0d745f49a7e
twedltsn <twedl@tsn.at>
2021-01-28 18:33:59 +0100

    log level info

--- src/logging.ts
+++ src/logging.ts
@@ -3,6 +3,6 @@ import { configure } from 'log4js';
 configure({
     appenders: { default: { type: `console` } },
     categories: {
-        default: { appenders: [`default`], level: process.env.LOG_LEVEL || `debug` },
+        default: { appenders: [`default`], level: process.env.LOG_LEVEL || `info` },
     },
 });
